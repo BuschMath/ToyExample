29d27f3fde7583490bfae5de4a57209d4fdf3fc6
BuschMath <busch@iowacentral.edu>
2023-09-13 10:40:16 -0500

    Arithmetic

--- ToyExample/main.cpp
+++ ToyExample/main.cpp
@@ -9,6 +9,32 @@ int main()
 	// Function call
 	printHelloWorld();
 
+	float a = 0.1;
+
+	int count = 0;
+	while (count < 10)
+	{
+		a = a + 0.1;
+		count++;
+	}
+
+	if (a == 1.0)
+	{
+		std::cout << "a is 1.0" << std::endl;
+	}
+	else
+	{
+		std::cout << "a is not 1.0" << std::endl;
+	}
+
+	std::cout << "Int division: " << (- 7) / 4 << std::endl;
+	std::cout << "Float division: " << 7.0 / 4.0 << std::endl;
+	std::cout << "Mod division: " << 7 % 4 << std::endl;
+
+	unsigned int b = 2147483647;
+
+	std::cout << "b: " << b + 2147483647 << std::endl;
+
 	return 0;
 }
 
